fd3f5c53f0ddf585c23d2d5119c1f58ce6f65a89
Sunha.Park <sunha321@gmail.com>
2021-07-07 17:23:50 +0900

    2021-07-07

--- reservations/models.py
+++ reservations/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+from . import managers
 
 
 class BookedDay(core_models.TimeStampedModel):
@@ -42,6 +43,7 @@ class Reservation(core_models.TimeStampedModel):
     room = models.ForeignKey(
         "rooms.Room", related_name="reservations", on_delete=models.CASCADE
     )
+    objects = managers.CustomReservationManager()
 
     def in_progress(self):
         now = timezone.now().date()
